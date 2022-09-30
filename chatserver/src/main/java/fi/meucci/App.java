5bec0a3e5c3b2854cf671b4932cd642db5086219
Fantaric <95363555+Fantaric@users.noreply.github.com>
2022-09-30 18:42:26 +0200

    Add files via upload

---
+++ chatserver/src/main/java/fi/meucci/App.java
@@ -0,0 +1,11 @@
+package fi.meucci;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Serverstr server = new Serverstr();
+        server.attendi();
+        server.comunica();
+    }
+}
---
+++ chatserver/src/main/java/fi/meucci/Serverstr.java
@@ -0,0 +1,59 @@
+package fi.meucci;
+
+import java.net.*;
+import java.io.*;
+
+
+public class Serverstr {
+    ServerSocket server = null;
+    Socket client = null;
+    String strRicevuta;
+    String strModificata;
+    BufferedReader in;
+    DataOutputStream out;
+
+    public Socket attendi()
+    {
+        try
+        {
+            System.out.println("Server partito in esecuzione");
+            server = new ServerSocket(6789);
+            client = server.accept();
+            server.close();
+
+
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new DataOutputStream(client.getOutputStream());
+
+        } catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server");
+            System.exit(1);
+        }
+        return client;
+    }
+
+    public void comunica()
+    {
+        try
+        {
+            System.out.println(" Benvenuto Client, scrivi una frase e la trasformo in maiuscolo");
+            strRicevuta = in.readLine();
+            System.out.println("Stringa ricevuta dal client: " +strRicevuta);
+
+            strModificata = strRicevuta.toUpperCase();
+            System.out.println("invio la stringa modificata al client");
+            out.writeBytes(strModificata + '\n');
+
+            System.out.println(" Server: fine elaborazione");
+            client.close();
+        } catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la comunicazione con il Client");
+            System.exit(1);
+        }
+    }
+
+}
